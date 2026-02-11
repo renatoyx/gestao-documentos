e03c54a067316bc31bb7492be8e0d22392fad822
Renato Araújo Gameiro <renatogameiro1@gmail.com>
2026-02-11 15:29:27 -0300

    setup inicial do projeto Spring Boot

---
+++ src/test/java/com/estagio/gestao_documentos/GestaoDocumentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estagio.gestao_documentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDocumentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
